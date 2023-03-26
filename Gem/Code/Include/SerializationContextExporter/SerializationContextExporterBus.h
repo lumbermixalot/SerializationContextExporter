c1126fdcfb8bac071f5027b3975d459e946668b5
lumbermixalot <lumbermixalot@github.com>
2023-03-26 11:36:35 -0500

    Improved comments.
    Signed-off-by: lumbermixalot <lumbermixalot@github.com>

--- Gem/Code/Include/SerializationContextExporter/SerializationContextExporterBus.h
+++ Gem/Code/Include/SerializationContextExporter/SerializationContextExporterBus.h
@@ -12,11 +12,21 @@ namespace SerializationContextExporter
         AZ_RTTI(SerializationContextExporterRequests, "{3AD84F22-E9A7-455E-9377-5E974B117CA7}");
         virtual ~SerializationContextExporterRequests() = default;
         
+        //! Returns a list of UUIDs for all classes whose name match the regular expression in @classNameRegexFilter.
+        //! If @fetchDependencies is true, it also adds the UUIDs of all the classes that are being referenced by the initial
+        // list of classes that matched the regular expression. The dependecies are returned regardless of their name.
         virtual AZStd::vector<AZ::Uuid> GetUuidsFromClassNameFilter(const AZStd::string& classNameRegexFilter, bool fetchDependencies) = 0;
-        virtual AZStd::string GetClassName(const AZ::Uuid& classUuid) = 0;
+
+        //! Give a class Uuid, returns a string, in json format, with all the data that is needed to serialize
+        //! deserialize a particular class.
         virtual AZStd::string GetClassInfoAsJson(const AZ::Uuid& classUuid) = 0;
 
-        virtual uint32_t GetMySerializedObject(const AZStd::string& forCrc32) = 0; // dummy function
+        //! Given a class Uuid, returns a string, with the class name.
+        virtual AZStd::string GetClassName(const AZ::Uuid& classUuid) = 0;
+
+        // Dummy function, left here for debugging purposes. Accepts
+        // a string as input and returns its 32bits CRC as Calculated by AZ::Crc32.
+        virtual uint32_t GetMySerializedObject(const AZStd::string& forCrc32) = 0;
     };
     
     class SerializationContextExporterBusTraits
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SerializationContextExporter
-An O3DE Gem that exports symbols in the serialization context according to user defined filters
+This is a Gem you can install to use with the O3DE Game Engine.
+Provides an EBus with Python bindings (SerializationContextExporterRequestBus) to extract the decription
+of any O3DE Class that is registered in the SerializationContext.
+
+In particular, the attached script: Gem/Editor/Scripts/o3de_classes_extractor.py
+was used to generate the `o3de_class.json` that is being used by  
+https://github.com/lumbermixalot/vscode-dbg-ext-o3de-lua/blob/main/O3DESerializationContextAutogen/src/main.ts  
+to generate:
+https://github.com/lumbermixalot/vscode-dbg-ext-o3de-lua/blob/main/src/o3deNetClasses/o3deClasses.ts
+
+
+
