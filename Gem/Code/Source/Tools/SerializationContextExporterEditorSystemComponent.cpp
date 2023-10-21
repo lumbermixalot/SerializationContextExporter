7c2feaa8ece4a7dc72d9d89d6181442818c66697
lumbermixalot <lumbermixalot@github.com>
2023-10-21 08:27:23 -0500

    Fixed a minor compilation bug.
    Signed-off-by: lumbermixalot <lumbermixalot@github.com>

--- Gem/Code/Source/Tools/SerializationContextExporterEditorSystemComponent.cpp
+++ Gem/Code/Source/Tools/SerializationContextExporterEditorSystemComponent.cpp
@@ -1,5 +1,12 @@
+/*
+* Copyright (c) Galib Arrieta (aka 'lumbermixalot@github', aka 'galibzon@github').
+* For complete copyright and license terms please see the LICENSE at the root of this distribution.
+*
+* SPDX-License-Identifier: Apache-2.0 OR MIT
+*
+*/
 
-#include <AzCore/Serialization/SerializeContext.h>
+#include <AzCore/RTTI/BehaviorContext.h>
 #include <AzCore/std/string/regex.h>
 #include <AzCore/JSON/document.h>
 #include <AzCore/JSON/stringbuffer.h>
